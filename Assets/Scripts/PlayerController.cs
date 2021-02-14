d20af01de8d3137ff31d5e9bf1255e28d837839f
William Barnes <jakebarnes1989@gmail.com>
2021-02-14 15:01:14 -0500

    Player and Camera Movement
    - Added player movement
    - Added camera rotation
    - Player should now roll in direction camera is pointing.
    - Player can now boost by using shift, still needs cool down.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private GameObject focalPoint;
+    private float speed = 5.0f;
+    private float boostPow = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetButton("Vertical") && Input.GetButton("Boost"))
+        {
+            playerRB.AddForce(focalPoint.transform.forward * verticalInput * speed * boostPow);
+        } else if (Input.GetButton("Vertical"))
+        {
+            playerRB.AddForce(focalPoint.transform.forward * verticalInput * speed);
+        }
+
+    }
+}
